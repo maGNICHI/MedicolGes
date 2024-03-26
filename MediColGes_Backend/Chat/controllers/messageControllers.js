8650e54e9ad837fc5869d889e2ba49902c8d9780
maGNICHI <manar.gnichi@esprit.tn>
2024-03-26 02:57:21 +0100

    first integration backend

---
+++ MediColGes_Backend/Chat/controllers/messageControllers.js
@@ -0,0 +1,72 @@
+const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../../UserManagement/Model/User");
+const Chat = require("../models/chatModel");
+const {Buffer}=require('buffer');
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    //:chatId in routes //request params
+    const messages = await Message.find({ chat: req.params.chatId })
+    //utilisée pour remplacer l'ID de l'expéditeur par les inf
+      .populate("sender", "name email")
+      //utilisée pour remplacer l'ID du chat par les détails complets du chat 
+      .populate("chat");
+
+    res.json(messages);
+    
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const sendMessage = asyncHandler(async (req, res) => {
+
+  const { content, chatId, isMedia, buffer:b  } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+  //schema 
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+    isMedia,
+    buffer:b?b:null
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+
+    //populating the instance
+    message = await message.populate("sender", "name");
+    message = await message.populate("chat");
+
+    //populating with the user in that chat field of our message doc instance
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+
+    res.json(message);
+
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+/*const   deleteMessages = asyncHandler( async (req, res) => {
+  try {
+      await Messages.findOneAndDelete({_id: req.params.id, sender: req.user._id,chat: chatId})
+      res.json({msg: 'Delete Success!'})
+  } catch (err) {
+      return res.status(500).json({msg: err.message})
+  }
+});*/
+
+module.exports = { allMessages, sendMessage };
--- MediColGes_Backend/server.js
+++ MediColGes_Backend/server.js
@@ -4,17 +4,21 @@ require('dotenv').config();
 const db = require('./ConfigDB/connection');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const upload = require('./multer')
 const cloudinary= require('./cloudinary')
 const fs = require ('fs')
 
-app.listen(process.env.PORT, () => { 
+/*app.listen(process.env.PORT, () => { 
     console.log('server started on port ' + (process.env.PORT));
-});
+});*/
 db();
 
 app.use(express.json())
+app.use(bodyParser.json({ limit: '50mb' }));
+// Increase payload size limit for URL encoded data
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
   origin: 'http://localhost:3000' // Allow requests from localhost:3000
@@ -24,6 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // routes
+app.use("/api/chat", require("./Chat/routes/chatRoutes"));
+app.use("/api/message", require("./Chat/routes/messageRoutes"));
 app.use('/api/project', require('./ProjectManagement/Routes/Project'))
 app.use('/api/feedback', require('./ProjectManagement/Routes/Feedback'))
 app.use('/form', require('./FormManagement/routes/FormRoutes'));
@@ -59,3 +65,51 @@ app.use("/api/user", require("./UserManagement/Routes/User"));
 //         });
 //     }
 // });
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+//Socket.io est conf pour écouter les cnx sur le serveur Express. 
+//config spécifie un délai(120 000 ms)et autorise cnx depuis"localhost" en raison de la politique CORS.
+
+const io = require("socket.io")(server, {
+  pingTimeout: 120000,
+  cors: {
+    origin: "http://localhost:3000", 
+  },
+});
+//creer event : on et emit :appele event w yab3thou l serveur
+//utilise protocole socket
+//evenement
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
+  
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+     // if (user._id === newMessageReceived.data.sender._id) return;
+
+      //Send the message back to add it to the messages array:
+     // socket.in(newMessageReceived.room).emit("message received", {newMessageReceived:newMessageReceived.data,chat});
+    });
+  });
+  socket.off("setup", () => {
+    socket.leave(userData._id);
+  });
+
+});  
+
