1e493b38a44599647568db0d35d54307eb6550b7
amani-star <amani.thameur@esprit.tn>
2024-03-27 16:44:53 +0100

    AddReponseByForm

--- routes/FormReponse.js
+++ routes/FormReponse.js
@@ -3,20 +3,30 @@ const router = express.Router();
 const Form = require('../models/Forms');
 const Response = require('../models/Response');
 // const Forms = require('../models/Forms');
-
-
-//get
-router.get('/Reponse/:id', async (req, res) => {
+//getAll
+router.get('/responses/:formId', async (req, res) => {
     try {
-        const form = await Form.findById(req.params.id);
-        if (!form) {
-            return res.status(404).json({ error: 'Formulaire non trouvé' });
+      const formId = req.params.formId; // Récupérer l'ID du formulaire depuis les paramètres de l'URL
+      const responses = await Response.find({ form: formId }).populate('form');
+      res.json(responses);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Erreur lors de la récupération des réponses." });
+    }
+  });
+//get
+router.get('/:ReponseId', async (req, res) => {
+    try {
+        console.log("Recherche de réponse pour le formulaire avec l'ID :", req.params.ReponseId);
+
+        const responses = await Response.findOne({ form: req.params.ReponseId  }).exec();
+        if (!responses) {
+            return res.status(404).json({ error: 'Aucune réponse trouvée pour ce formulaire' });
         }
-        const responses = await Response.find({ form: req.params.id }).exec();
         res.status(200).json({
             status: 'success',
             data: {
-                responses,
+                response,
             },
         });
     } catch (error) {
